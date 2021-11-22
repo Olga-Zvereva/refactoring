3530fb16d35d8bd574eee82120b2623ea8fdc4b4
flexiG17 <mister23krut@gmail.com>
2021-11-22 15:38:47 +0500

    Этап 3
    Сомневаюсь в корректности логики программы, но главное, что понял как коммитить и прочее

--- filter.py
+++ filter.py
@@ -8,19 +8,22 @@ def pixel_filter(side: int, step: int):
         y = 0
         while y < height:
             result = 0
-            for x in range(x, x + side):
-                for y in range(y, y + side):
-                    result += sum(img_arr[x][y][range(3)] / 3)
+            for n, m in np.nditer([np.arange(x, x + side)[:, None], np.arange(y, y + side)]):
+                result += img_arr[n][m][0] / 3 + img_arr[n][m][1] / 3 + img_arr[n][m][2] / 3
             avg_brightness = int(result) // (side * side)
             pixel_coloring(avg_brightness, x, y, side, step)
             y += side
         x += side
 
 
-def pixel_coloring(avg_brightness, x, y, side, grayscale):
-    for x in range(x, x + side):
-        for y in range(y, y + side):
-            img_arr[x][y][range(3)] = int(avg_brightness // grayscale) * grayscale
+def pixel_coloring(x, y, side, step):
+    result = 0
+    for n, m in np.nditer([np.arange(x, x + side)[:, None], np.arange(y, y + side)]):
+        result += img_arr[n][m][0] / 3 + img_arr[n][m][1] / 3 + img_arr[n][m][2] / 3
+    avg_brightness = int(result) // (side * side)
+    for n in range(x, x + side):
+        for m in range(y, y + side):
+            img_arr[n][m][range(3)] = int(avg_brightness // step) * step
 
 np.seterr(over='ignore')
 img_arr = np.array(Image.open("img2.jpg"))
